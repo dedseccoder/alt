f74c83cc80397c25128f1b1246a33e5f7ca6c932
dedseccoder <andrey.flach2@gmail.com>
2023-06-16 09:42:09 +0300

    --

--- cli/main.c
+++ cli/main.c
@@ -47,7 +47,8 @@ int main(int argc, char **argv)
 	else{
 		info = GET_Export(url, argv[1], NULL);
 	}
-
+	url[0] = '\0';
+	strcat(url, "https://rdb.altlinux.org/api/export/branch_binary_packages/");
 	list jsonInfoList1, jsonInfoList2;
 
     InitList(&jsonInfoList1);
@@ -67,7 +68,6 @@ int main(int argc, char **argv)
 		pushList(&jsonInfoList1, data, buildtime, version);
 		i++;
 	}
-	free(info);
 
 	index = 0;
 	names = 0;
@@ -96,17 +96,21 @@ int main(int argc, char **argv)
 		pushList(&jsonInfoList2, data, buildtime, version);
 		i++;
 	}
-	free(info2);
-
 	list onlyA, onlyB, freshList;
 	InitList(&onlyA);
 	InitList(&onlyB);
 	InitList(&freshList);
-
+	printf("Sorting...\n");
 	getOnly(&jsonInfoList1, &jsonInfoList2, &onlyA);
 	getOnly(&jsonInfoList2, &jsonInfoList1, &onlyB);
 	getFreshest(&jsonInfoList1, &jsonInfoList2, &freshList);
-	writeJsonFile(&onlyA, &onlyB, &freshList, argv[3], argv[1], argv[2]);
+	printf("Writing JSON file...\n");
+	if(argv[3]){
+		writeJsonFile(&onlyA, &onlyB, &freshList, argv[1], argv[2], argv[3]);
+	}
+	else{
+		writeJsonFile(&onlyA, &onlyB, &freshList, argv[1], argv[2], NULL);
+	}
 
 	return 0;
 }
@@ -138,14 +142,21 @@ void printList(list *sList)
     }
 }
 
-void writeJsonFile(list* onlyA, list* onlyB, list* freshList, char* arch, char* branchA, char* branchB)
+void writeJsonFile(list* onlyA, list* onlyB, list* freshList, char* branchA, char* branchB, char* _arch)
 {
 	node *p1 = onlyA->tail;
 	node *p2 = onlyB->tail;
 	node *p3 = freshList->tail;
 	char filename[1024];
 	strcat(filename, "alt-export-");
-	strcat(filename, arch);
+	if(_arch){
+		strcat(filename, _arch);
+	}
+	else{
+		strcat(filename, branchA);
+		strcat(filename, "_");
+		strcat(filename, branchB);
+	}
 	strcat(filename, ".json");
 	FILE *pf = fopen(filename, "w");
 	fprintf(pf, "{\n");
@@ -182,7 +193,7 @@ void writeJsonFile(list* onlyA, list* onlyB, list* freshList, char* arch, char* 
 
 	fprintf(pf, "\n}");
 	fclose(pf);
-	printf("JSON file created");
+	printf("JSON file complete\n");
 }
 
 void getOnly(list* jsonInfo1, list* jsonInfo2, list* only)
